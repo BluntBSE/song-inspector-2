1dfe7164e07abd82ebef91f75a7e9c1fd494c3e4
Blunt <ram.news1@gmail.com>
2023-06-17 18:30:48 -0400

    Auth and Spotify API

--- server/app/apis/secrets.py
+++ server/app/apis/secrets.py
@@ -1,7 +1,7 @@
 import requests
 
-client_id = "67f7152ee71e474a912d1d11e106895d"
-client_secret = "eb31a88f292140718f2b3d26d96ffc7b"
+client_id = "secret here"
+client_secret = "secret here"
 
 def generateToken():
     token = requests.post('https://accounts.spotify.com/api/token', data={'grant_type': 'client_credentials'}, auth=(client_id, client_secret)).json()
